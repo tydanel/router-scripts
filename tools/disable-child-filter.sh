f4ad239fa7e8bf1218a25b682740ebe94674fbee
Tyrell Daniel Nelson <ty.dan.nel@gmail.com>
2023-02-21 17:20:10 +0000

    Changed username for scripts

--- tools/disable-child-filter.sh
+++ tools/disable-child-filter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-ssh -i $HOME/.ssh/id_rsa tyrell@router.lan /system/script/run disable-kids-filter-rules 
+ssh -i $HOME/.ssh/id_rsa_scripty scripty@router.lan /system/script/run disable-kids-filter-rules 
